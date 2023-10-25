6e6a2b7f283e39d50ce29e958b1fda67248e20b2
Bruce Wheeler <bruce.wheeler18@alumni.colostate.edu>
2023-10-25 08:02:01 -0600

    Trying to add buttton clear function

--- You Are Awesome2!/ViewController.swift
+++ You Are Awesome2!/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var messageLabel2: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("View did load")
+
     }
 
+    @IBAction func messageButtonPressed(_ sender: Any) {
+        
+        print("Message button pressed")
+        
+        messageLabel.text = "Excellent!"
+        messageLabel.textColor = UIColor.white
+        messageLabel.textAlignment = .center
+        
+        messageLabel2.text = "You Got This!"
+        messageLabel2.textColor = UIColor.white
+        messageLabel2.textAlignment = .center
+        
+    }
+
+    @IBAction func messageButtonPressed2(_ sender: UIButton) {
+        
+        messageLabel.text = ""
+        messageLabel2.text = ""
+        
+    }
+    
+
+    
 
 }
 
